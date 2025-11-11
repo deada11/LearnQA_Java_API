26023a179d494ca5150c8642034ad0d8877fc298
pnikitin <pnikitin@moysklad.ru>
2025-11-11 14:32:59 +0300

    Ex16: Get user details for another user

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -3,14 +3,18 @@ package tests;
 import constants.Constants;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
+import lib.DataGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserGetTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
 
     @Test
     public void testGetUserDataNotAuth(){
@@ -49,5 +53,27 @@ public class UserGetTest extends BaseTestCase {
 
         String[] expectedFields = {"username", "firstName", "lastName", "email"};
         Assertions.assertJsonHasFields(responseUserData, expectedFields);
+    }
+
+    @Test
+    public void testGetUserDetailsAuthAsAnotherUser(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+        authData = DataGenerator.getRegistrationData(authData);
+
+        Response responseGetAuth = apiCoreRequests.makePostRequest(Constants.URL + "/user/login", authData);
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+        int userId = this.getIntFromJson(responseGetAuth, "user_id");
+
+        Response responseUserData = apiCoreRequests.makeGetRequest(
+                Constants.URL + "/user/" + ++userId,
+                header,
+                cookie);
+
+        String expectedField = "username";
+        Assertions.assertJsonHasField(responseUserData, expectedField);
     }
 }
